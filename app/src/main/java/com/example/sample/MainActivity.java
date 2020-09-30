784d8730a1161da60152995cda5627b26b535182
Himanshu Khadke <khadkeh754@gmail.com>
2020-09-30 10:40:44 +0530

    My first Commit

---
+++ app/src/main/java/com/example/sample/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    public int count = 0;
+    Button order;
+    TextView items;
+    TextView price;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        items = findViewById(R.id.items);
+        price = findViewById(R.id.price);
+        order = (Button) findViewById(R.id.order_btn);
+        order.setOnClickListener(v -> updateItems());
+    }
+
+    private void updateItems() {
+        items.setText(String.valueOf(++count));
+        updatePrice();
+    }
+
+    private void updatePrice() {
+        price.setText(NumberFormat.getCurrencyInstance().format(count * 10));
+    }
+}
